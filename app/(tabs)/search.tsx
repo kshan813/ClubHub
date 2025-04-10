8ca2c36cfea5487451ee7dddc5612f04ec61a310
Jarod Houston <jarodhouston26@gmail.com>
2025-04-10 11:22:31 -0700

    Bare bones search dropdown

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,48 +1,147 @@
-import React from 'react';
-import { View, ScrollView, Text} from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import HorizontalButtonList from './../../components/ScrollClubs'; 
-
-
+import { Layout } from "@react-navigation/bottom-tabs/lib/typescript/commonjs/src/types";
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  Keyboard,
+  TouchableWithoutFeedback,
+  LayoutChangeEvent,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
+import HorizontalButtonList from "./../../components/ScrollClubs";
 
 export default function TabThreeScreen() {
-  const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchSelected, setSearchSelected] = useState(false);
+  const [searchbarLayout, setSearchbarLayout] = useState<{
+    y: number;
+    height: number;
+  } | null>(null);
+
   const handleButtonClick = (item: string) => {
     console.log(`Button clicked: ${item}`);
+    Keyboard.dismiss();
   };
 
+  const handleSearchFocus = () => {
+    console.log("Search bar in focus");
+    setSearchSelected(true);
+  };
 
-  const BusinessButtons = ['The Bruin Group', 'Bruin Consulting', 'Alpha Kappa Psi', 'Sigma Eta Pi', 'Bruin Asset Management'];
-  const TechButtons = ['ACM-W', 'UPE', 'Dev X', 'LAHacks', 'Bruin AI'];
-  const AthleticsButtons = ['Bruin Run', 'Women\'s Club Soccer', 'Mens\'s Club Lacrosse', 'Women\'s Club Waterpolo', 'Olympic Weightlifting'];
+  const handleSearchBlur = () => {
+    console.log("Search bar out of focus");
+    setSearchSelected(false);
+  };
+
+  const handleSearchLayout = (event: LayoutChangeEvent) => {
+    const { y, height } = event.nativeEvent.layout;
+    setSearchbarLayout({ y, height });
+  };
+
+  const searchDropDownHeight = searchbarLayout
+    ? searchbarLayout.y - searchbarLayout.height / 2
+    : 0;
+
+  const BusinessButtons = [
+    "The Bruin Group",
+    "Bruin Consulting",
+    "Alpha Kappa Psi",
+    "Sigma Eta Pi",
+    "Bruin Asset Management",
+  ];
+  const TechButtons = ["ACM-W", "UPE", "Dev X", "LAHacks", "Bruin AI"];
+  const AthleticsButtons = [
+    "Bruin Run",
+    "Women's Club Soccer",
+    "Mens's Club Lacrosse",
+    "Women's Club Waterpolo",
+    "Olympic Weightlifting",
+  ];
+
+  const tags = [
+    "Business",
+    "Engineering",
+    "Chemistry",
+    "Computer Science",
+    "Biology",
+    "Writing",
+    "Sports",
+    "Music",
+    "Art",
+    "Neuroscience",
+  ];
 
   return (
-     <View style={{ flex: 1, paddingTop: '15%', backgroundColor: '#CADCE8'}}>
-       <Searchbar style={{ width: '90%', alignSelf: 'center', backgroundColor: '#FDFDFD'}}
-       placeholder="Search"
-       onChangeText={setSearchQuery}
-       value={searchQuery}
-      />
-      <Text style= {{ fontSize: 30,fontWeight: 'bold', color: '#1E1E1E', marginTop: 15, textAlign: 'left', marginLeft: 10, marginRight: 10}}>
-        Explore
-      </Text>    
-      <ScrollView>
-        <HorizontalButtonList 
-          sectionTitle= "Business"
-          buttonData={BusinessButtons}
-          onClick={handleButtonClick}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={{ flex: 1, paddingTop: "15%", backgroundColor: "#CADCE8" }}>
+        <Searchbar
+          style={{
+            width: "90%",
+            alignSelf: "center",
+            backgroundColor: "#FDFDFD",
+          }}
+          placeholder="Search"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          onFocus={handleSearchFocus}
+          onBlur={handleSearchBlur}
+          onLayout={handleSearchLayout}
         />
-        <HorizontalButtonList 
-          sectionTitle= "Tech"
-          buttonData={TechButtons}
-          onClick={handleButtonClick}
-        />
-        <HorizontalButtonList 
-          sectionTitle= "Athletics"
-          buttonData={AthleticsButtons}
-          onClick={handleButtonClick}
-        />
-      </ScrollView>
-    </View>
+        {searchSelected && (
+          <View
+            style={{
+              top: searchDropDownHeight,
+              width: "90%",
+              backgroundColor: "#FDFDFD",
+              alignSelf: "center",
+              paddingTop: searchDropDownHeight * -1,
+              paddingBottom: 14,
+              paddingHorizontal: 20,
+              zIndex: -1,
+              borderBottomLeftRadius: 28,
+              borderBottomRightRadius: 28,
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
+          >
+            {tags.map((word, index) => (
+              <Text key={index}>{word}</Text>
+            ))}
+          </View>
+        )}
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            color: "#1E1E1E",
+            marginTop: 15,
+            textAlign: "left",
+            marginLeft: 10,
+            marginRight: 10,
+          }}
+        >
+          Explore
+        </Text>
+        <ScrollView>
+          <HorizontalButtonList
+            sectionTitle="Business"
+            buttonData={BusinessButtons}
+            onClick={handleButtonClick}
+          />
+          <HorizontalButtonList
+            sectionTitle="Tech"
+            buttonData={TechButtons}
+            onClick={handleButtonClick}
+          />
+          <HorizontalButtonList
+            sectionTitle="Athletics"
+            buttonData={AthleticsButtons}
+            onClick={handleButtonClick}
+          />
+        </ScrollView>
+      </View>
+    </TouchableWithoutFeedback>
   );
-};
+}
--- components/ScrollClubs.tsx
+++ components/ScrollClubs.tsx
@@ -1,37 +1,45 @@
-import React from 'react';
-import { FlatList, TouchableOpacity, Text, StyleSheet, View} from 'react-native';
+import React from "react";
+import {
+  FlatList,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  View,
+} from "react-native";
 
 interface HorizontalButtonListProps {
   sectionTitle: string;
   buttonData: string[];
-  onClick: (item: string) => void; 
+  onClick: (item: string) => void;
 }
 
-const HorizontalButtonList: React.FC<HorizontalButtonListProps> = ({ sectionTitle, buttonData, onClick }) => {
+const HorizontalButtonList: React.FC<HorizontalButtonListProps> = ({
+  sectionTitle,
+  buttonData,
+  onClick,
+}) => {
   const handlePress = (item: string) => {
     onClick(item);
   };
 
   return (
     <View>
-    <Text style={styles.sectionTitle}>{sectionTitle}</Text>
-    <FlatList
-      data={buttonData} 
-      horizontal={true}
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item, index) => index.toString()}
-      contentContainerStyle={{ paddingHorizontal: 10, marginTop: 5 }}
-      renderItem={({ item }) => (
-        <TouchableOpacity
-          style={[
-            styles.button,
-          ]}
-          onPress={() => handlePress(item)}
-        >
-          <Text style={styles.buttonText}>{item}</Text>
-        </TouchableOpacity>
-      )}
-    />
+      <Text style={styles.sectionTitle}>{sectionTitle}</Text>
+      <FlatList
+        data={buttonData}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={{ paddingHorizontal: 10, marginTop: 5 }}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={[styles.button]}
+            onPress={() => handlePress(item)}
+          >
+            <Text style={styles.buttonText}>{item}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
@@ -44,27 +52,28 @@ const styles = StyleSheet.create({
     width: 168,
     marginRight: 8,
     borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FDFDFD',
-    shadowColor: '#000',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FDFDFD",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 2,
   },
   buttonText: {
-    color: '#1E1E1E',
+    color: "#1E1E1E",
     fontSize: 16,
+    textAlign: "center",
   },
   sectionTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#1E1E1E',
+    fontWeight: "bold",
+    color: "#1E1E1E",
     marginTop: 15,
-    textAlign: 'left',
+    textAlign: "left",
     marginLeft: 10,
-    marginRight: 10
+    marginRight: 10,
   },
 });
 
