56b65d18c23ee7b3d19a403c86a5f6173647b145
Jarod Houston <jarodhouston26@gmail.com>
2025-05-14 20:02:10 -0700

    Fixed favorites page like a GOAT

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { View, Text } from "react-native";
-import { doc, getDoc } from "firebase/firestore";
+import { ScrollView, View, Text } from "react-native";
+import { doc, getDoc, getDocs, collection } from "firebase/firestore";
 import { db } from "../firebase";
 
 export default function TabFourScreen() {
@@ -10,15 +10,24 @@ export default function TabFourScreen() {
   useEffect(() => {
     const fetchClub = async () => {
       try {
-        const docRef = doc(db, "clubs", "CfL2RFYWH3V4UMIblCoN");
-        const docSnap = await getDoc(docRef);
+        const clubsCollectionRef = collection(db, "clubs");
+        const querySnapshot = await getDocs(clubsCollectionRef);
+        // const docRef = doc(db, "clubs", "CfL2RFYWH3V4UMIblCoN");
+        // const docSnap = await getDoc(docRef);
 
-        if (docSnap.exists()) {
-          console.log("Document data:", docSnap.data());
-          setClubData(docSnap.data());
-        } else {
-          console.log("No such document!");
-        }
+        const clubsData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setClubData(clubsData);
+
+        // if (docSnap.exists()) {
+        //   console.log("Document data:", docSnap.data());
+        //   setClubData(docSnap.data());
+        //   console.log(docSnap.data());
+        // } else {
+        //   console.log("No such document!");
+        // }
       } catch (error) {
         console.error("Error fetching document:", error);
       }
@@ -28,17 +37,65 @@ export default function TabFourScreen() {
   }, []); // empty deps = run once on mount
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Explore Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#CADCE8",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 24,
+          marginTop: 100,
+          alignSelf: "left",
+          marginLeft: 30,
+          fontWeight: "bold",
+        }}
+      >
+        Favorites
+      </Text>
       {clubData ? (
-        <View>
-          {Object.entries(clubData).map(([key, value]) => (
-            <Text key={key} style={{ marginBottom: 5 }}>
-              <Text style={{ fontWeight: "bold" }}>{key}: </Text>
-              {value?.toString()}
-            </Text>
+        <ScrollView style={{ marginTop: 30 }}>
+          {clubData.map((club: any) => (
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "70%",
+                gap: 20,
+                marginLeft: 50,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "white",
+                  width: 40,
+                  height: 40,
+                  borderRadius: 25,
+                  marginBottom: 14,
+                }}
+              ></View>
+              <View
+                key={club.id}
+                style={{
+                  marginBottom: 20,
+                  borderColor: "white",
+                  borderStyle: "dashed",
+                  borderWidth: 2,
+                  padding: 10,
+                  width: "100%",
+                }}
+              >
+                <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                  {club.name}
+                </Text>
+                <Text>{club.description}</Text>
+              </View>
+            </View>
           ))}
-        </View>
+        </ScrollView>
       ) : (
         <Text>Loading...</Text>
       )}
